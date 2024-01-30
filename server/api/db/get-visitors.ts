4c2fddff09a9d188acf94d2268adf42d8768272d
yunyuyuan <me@yunyuyuan.net>
2024-01-30 19:23:05 +0800

    test

--- server/api/db/get-visitors.ts
+++ server/api/db/get-visitors.ts
@@ -1,9 +1,38 @@
 export default defineEventHandler(async (event) => {
-  // return await getVisitors();
-
-  const url = "http://ip.jsontest.com/";
-
-  return await event.$fetch(url, {
-    method: "get"
-  });
+  return await getVisitors();
 });
+
+async function request(path: string, data: any) {
+  if (!process.env.MONGODB_PWD || !process.env.MONGODB_USER) {
+    throw new Error("Need Mongodb Atlas Authentication");
+  }
+
+  const url = process.env.MONGODB_ENDPOINT + path;
+  const requestData = {
+    ...data,
+    dataSource: process.env.MONGODB_DATA_SOURCE,
+    database: "nuxt3-blog",
+    collection: "visitors"
+  };
+
+  const res = await $fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      email: process.env.MONGODB_USER,
+      password: process.env.MONGODB_PWD
+    },
+    body: requestData
+  });
+  return res as any;
+};
+
+export async function getVisitors () {
+  const res = await request("/action/find", {
+    filter: {
+      ntype: "/articles"
+    },
+    projection: { _id: 0, nid: 1, nvisitors: 1 }
+  });
+  return res.documents;
+}
